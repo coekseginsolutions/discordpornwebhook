2d66ca5b244355b9dede1076a5a0e9f50ca4b32d
coekseginsolutions <129398637+coekseginsolutions@users.noreply.github.com>
2023-03-30 14:59:04 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,33 @@
+from pornhub_api import PornhubApi
+import asyncio
+import random
+import aiohttp
+from pornhub_api.backends.aiohttp import AioHttpBackend
+from discord_webhook import DiscordWebhook
+
+async def execute():
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+        backend = AioHttpBackend()
+        backend._session = session
+        api = PornhubApi(backend=backend)
+        video = await api.video.get_by_id("ph560b93077ddae")
+
+
+asyncio.run(execute())
+
+api = PornhubApi()
+
+tags = random.sample(api.video.tags("f").tags, 5)
+category = random.choice(api.video.categories().categories)
+result = api.search.search_videos(ordering="mostviewed", tags=tags, category=category)
+
+print(result.size())
+for vid in result:
+    print(vid.title, vid.url)
+    break
+    
+webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1090400465344274483/RlfUU9_V2yjLK5MkseqN1vAk0T2UrG7CXqCUtSbQmt8vzCvRqzR2vhIu7bMmKBOb2kae')
+
+message = f"New Video: {vid.title}\n{vid.url}"
+webhook.set_content(content=message)
+response = webhook.execute()
